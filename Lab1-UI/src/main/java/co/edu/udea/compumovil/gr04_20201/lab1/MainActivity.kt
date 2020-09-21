6a3ea3cb0d75f407a2cee5a00eff396046254549
leonel1221 <lzm_21@hotmail.com>
2020-09-20 21:03:10 -0500

    personal data adelantado

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20201/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20201/lab1/MainActivity.kt
@@ -1,11 +1,20 @@
 package co.edu.udea.compumovil.gr04_20201.lab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnIniciar.setOnClickListener {
+            val intent:Intent= Intent(this, PersonalData::class.java)
+            startActivity(intent)
+        }
     }
+
 }
